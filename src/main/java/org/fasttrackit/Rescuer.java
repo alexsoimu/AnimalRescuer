50535d5e329b6daf1611be410ad424ac9f457435
Alex Soimu <fasttrackit.java.online@gmail.com>
2022-09-08 12:30:18 +0300

    08.09.2022

--- src/main/java/org/fasttrackit/Rescuer.java
+++ src/main/java/org/fasttrackit/Rescuer.java
@@ -30,12 +30,26 @@ public class Rescuer {
         animal.setHungerLevel(hungerLevel);
 
         System.out.println(animal.getName() + "'s hunger level is now: " + animal.getHungerLevel());
+
+        if (food.getName().equals(animal.getFavoriteFoodName())) {
+            animal.setHappinessLevel(animal.getHappinessLevel() + 1);
+            System.out.println(animal.getName() + "'s happiness level is now " + animal.getHappinessLevel());
+        }
+
     }
 
     public void entertain(Animal animal, EntertainmentActivity activity) {
         System.out.println(this.name + " just did/played some " + activity.getName() + " with " + animal.getName());
 
-        animal.setHappinessLevel(animal.getHappinessLevel() + 1);
+        int increment;
+
+        if (activity.getName().equals(animal.getFavoriteActivityName())){
+            increment = 2;
+        } else {
+            increment = 1;
+        }
+
+        animal.setHappinessLevel(animal.getHappinessLevel() + increment);
 
         System.out.println(animal.getName() + "'s happiness level is now: " + animal.getHappinessLevel());
     }
